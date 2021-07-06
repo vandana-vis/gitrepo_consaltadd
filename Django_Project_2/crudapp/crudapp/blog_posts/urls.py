c1456e2c113fb6a9f30d7798daa607bdfa5e54a9
vandana-vis <vandana.penamakuru@gmail.com>
2021-07-06 00:18:28 -0400

    django_project_2 updated

--- Django_Project_2/crudapp/crudapp/blog_posts/urls.py
+++ Django_Project_2/crudapp/crudapp/blog_posts/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('view/<int:pk>', views.book_view, name='book_view'),
+    path('edit/<int:pk>', views.book_update, name='book_edit'),
+    path('delete/<int:pk>', views.book_delete, name='book_delete'),
 
 ]
--- Django_Project_2/crudapp/crudapp/blog_posts/views.py
+++ Django_Project_2/crudapp/crudapp/blog_posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
+from .forms import BookForm
 
 
 def book_list(request):
@@ -10,3 +11,20 @@ def book_list(request):
 def book_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'blog_posts/post_form.html', {'object': book})
+
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, 'blog_posts/post_form2.html', {'form': form})
+
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'blog_posts/post_delete.html', {'object': book})
